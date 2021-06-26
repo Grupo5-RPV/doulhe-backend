72e07dd5aecf9ad82d86b625bc3c87f6cf1a08ff
Guilherme Samuel <guilhermesamuel79@gmail.com>
2021-06-25 23:46:19 -0300

    feat: factories for create auction controller

---
+++ src/main/factories/create-auction/create-auction-factory.ts
@@ -0,0 +1,19 @@
+import DbCreateAuction from '../../../data/usecases/db-create-auction'
+import AuctionItemRepository from '../../../infra/repositories/auction-item-repository'
+import AuctionRepository from '../../../infra/repositories/auction-repository'
+import CreateAuctionController from '../../../presentation/controllers/create-auction-controller'
+import Controller from '../../../presentation/protocols/controller'
+import Validation from '../../../presentation/protocols/validation'
+
+class SomeValidation implements Validation {
+  validate (input: any): Error {
+    throw new Error('Method not implemented.')
+  }
+}
+
+export const createAuctionFactory = (): Controller => {
+  const auctionRepository = new AuctionRepository()
+  const auctionItemRepository = new AuctionItemRepository()
+  const dbCreateAuction = new DbCreateAuction(auctionRepository, auctionItemRepository)
+  return new CreateAuctionController(new SomeValidation(), dbCreateAuction)
+}
