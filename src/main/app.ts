6e7b94a6a4813d7b5bc56e446a11953ad6604da2
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-06-27 13:41:11 -0300

    feat: configured application routes

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import router from './routes/auction-routes'
+import setupRoute from './config/routes'
 
 const app = express()
-
 app.use(express.json())
-app.use(router)
+//app.use(router)
+
+setupRoute(app)
 
 export default app
