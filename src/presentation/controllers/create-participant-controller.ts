4663d9135fe2e34dfa73b650f403dafdc033b0b4
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-07-06 04:08:53 -0300

    feat: created controller to create-participant

---
+++ src/presentation/controllers/create-participant-controller.ts
@@ -0,0 +1,44 @@
+import CreateParticipant from "../../domain/usecases/participant/create-participant";
+import InvalidParamError from "../errors/invalid-param-error";
+import { badRequest, ok, serverError } from "../helpers/http-helper";
+import Controller from "../protocols/controller";
+import httpRequest from "../protocols/http-request";
+import httpResponse from "../protocols/http-response";
+import Validation from "../protocols/validation";
+
+export default class CreateParticipantController implements Controller {
+    constructor(
+        private validator: Validation,
+        private createParticipant: CreateParticipant
+    ) {
+        this.validator = validator,
+            this.createParticipant = createParticipant;
+    }
+
+    async handle(httpRequest: httpRequest): Promise<httpResponse> {
+        try {
+            const error = this.validator.validate(httpRequest.body)
+            if (error) {
+                return badRequest(error)
+            }
+            const { id, name, username, password, email, address, phone } = httpRequest.body
+            const participant = await this.createParticipant.create({
+                id,
+                name,
+                username,
+                password,
+                email,
+                address,
+                phone
+            })
+            return ok(participant)
+        } catch (error) {
+            console.log(error)
+            if(error instanceof InvalidParamError){
+                return badRequest(error)
+            }
+            return serverError(error)
+        }
+    }
+
+}
