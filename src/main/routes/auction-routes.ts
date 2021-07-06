4c767a37af2c9294e1b20c0f29eb6c33001fb434
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-07-06 04:07:15 -0300

    feat: added route /participants

--- src/main/routes/auction-routes.ts
+++ src/main/routes/auction-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { createAuctionFactory } from '../factories/create-auction/create-auction-factory'
+import { createParticipantFactory } from '../factories/create-participant/create-participant-factory'
 
 const router = Router()
 
 router.post('/auctions', adaptRoute(createAuctionFactory()))
-
+router.post('/participants',adaptRoute(createParticipantFactory()))
 export default router
