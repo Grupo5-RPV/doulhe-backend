b35517e87e4f7213448c1098faff836a5f1d266f
Guilherme Samuel <guilhermesamuel79@gmail.com>
2021-07-05 21:38:12 -0300

    refact: moving db infra files to typeorm directory

--- src/infra/typeorm/repositories/auction-item-repository.ts
+++ src/infra/typeorm/repositories/auction-item-repository.ts
@@ -1,7 +1,7 @@
 import { getRepository } from 'typeorm'
 
-import { AuctionItem } from '../../domain/entities'
-import IAuctionItemRepository from '../../data/protocols/db/auction-item-repository'
+import { AuctionItem } from '../../../domain/entities'
+import IAuctionItemRepository from '../../../data/protocols/db/auction-item-repository'
 
 export default class AuctionItemRepository implements IAuctionItemRepository {
   async findById (auctionItemId: string): Promise<AuctionItem> {
@@ -9,7 +9,7 @@ export default class AuctionItemRepository implements IAuctionItemRepository {
   }
 
   async updateAuctionId (auctionId: string, auctionItemId: string): Promise<AuctionItem> {
-    const auctionItem = await getRepository(AuctionItem).findOne({ id: auctionItemId })
+    const auctionItem = await this.findById(auctionItemId)
     auctionItem.auctionId = auctionId
     return await getRepository(AuctionItem).save(auctionItem)
   }
--- src/infra/typeorm/repositories/auctioneer-repository.ts
+++ src/infra/typeorm/repositories/auctioneer-repository.ts
@@ -1,7 +1,7 @@
-import IAuctioneerRepository from 'src/data/protocols/db/auctioneer-repository'
+import IAuctioneerRepository from '../../../data/protocols/db/auctioneer-repository'
 import { getRepository } from 'typeorm'
 
-import { Auctioneer } from '../../domain/entities'
+import { Auctioneer } from '../../../domain/entities'
 
 export default class AuctioneerRepository implements IAuctioneerRepository {
   async findById (id: string): Promise<Auctioneer> {
