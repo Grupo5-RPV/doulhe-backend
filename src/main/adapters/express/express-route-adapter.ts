fa966d5ff947fed6896d1c30eb68fe733ff40c62
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-07-09 20:13:27 -0300

    feat: added value to params field

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -5,8 +5,10 @@ import HttpRequest from '../../../presentation/protocols/http-request'
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
-      body: req.body
+      body: req.body,
+      params: req.params
     }
+    
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
