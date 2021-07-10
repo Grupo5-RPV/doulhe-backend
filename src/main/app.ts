d3d4dee600a9c10c1b7d00efa890cf27859a290d
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-07-09 22:30:05 -0300

    fix: used participant-router in app

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import auctionRoutes from './routes/auction-routes'
+import participantRouter from './routes/participant-router'
 
 const app = express()
 app.use(express.json())
 app.use(auctionRoutes)
+app.use(participantRouter)
 
 export default app
