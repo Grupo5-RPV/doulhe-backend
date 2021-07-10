f5e48503b3d14fcef8ec1722490bb583d5b54d67
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-07-09 22:17:45 -0300

    feat: added field token

--- src/domain/usecases/participant/create-participant-params.ts
+++ src/domain/usecases/participant/create-participant-params.ts
@@ -1,9 +1,10 @@
 export default interface ICreatePaticipantParams {
-    id: string;
-    name: string;
-    username: string;
-    password: string;
-    email: string;
-    address: string;
-    phone: string;
+    id: string
+    name: string
+    username: string
+    password: string
+    email: string
+    address: string
+    phone: string
+    token: string
   }
