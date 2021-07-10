0b8891e830e9a6fae640a9e614858ea1a94cd4d2
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-07-09 22:27:13 -0300

    refact: moved routes of participants to file participants-routes

---
+++ src/main/routes/participant-router.ts
@@ -0,0 +1,9 @@
+import {Router} from 'express'
+import { adaptRoute } from '../adapters/express/express-route-adapter'
+import { createParticipantFactory } from '../factories/create-participant/create-participant-factory'
+
+const router = Router()
+
+router.post('/participants',adaptRoute(createParticipantFactory()))
+
+export default router
