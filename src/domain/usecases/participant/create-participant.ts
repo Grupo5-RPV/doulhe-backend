68fe91595d820bae36c3b534c4e3ab050f46fd27
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-07-10 17:35:57 -0300

    feat: migration auctionBidTable

--- src/domain/usecases/participant/create-participant.ts
+++ src/domain/usecases/participant/create-participant.ts
@@ -1,32 +1,31 @@
-import { IdGenerator } from "../../../data/protocols/identification/id-generator"
-import IParticipantRepository from "../../../data/protocols/db/participant-repository"
-import InvalidParamError from "../../../presentation/errors/invalid-param-error"
-import { Participant } from "../../../domain/entities"
-import Hasher from "../../../data/protocols/criptography/hasher"
-import ICreatePaticipantParams from "./create-participant-params"
+import { IdGenerator } from '../../../data/protocols/identification/id-generator'
+import IParticipantRepository from '../../../data/protocols/db/participant-repository'
+import InvalidParamError from '../../../presentation/errors/invalid-param-error'
+import { Participant } from '../../../domain/entities'
+import Hasher from '../../../data/protocols/criptography/hasher'
+import ICreatePaticipantParams from './create-participant-params'
 
-export default class CreateParticipant{
-
-    constructor(
+export default class CreateParticipant {
+  constructor (
         private participantRepository : IParticipantRepository,
         private idGenerator : IdGenerator,
         private hasher : Hasher
-    ){
-        this.participantRepository = participantRepository
-        this.idGenerator = idGenerator
-        this.hasher = hasher;
+  ) {
+    this.participantRepository = participantRepository
+    this.idGenerator = idGenerator
+    this.hasher = hasher
+  }
+
+  async create (participantData : ICreatePaticipantParams): Promise<Participant> {
+    participantData.id = this.idGenerator.createUUID()
+
+    const participantExistis = await this.participantRepository.findByEmail(participantData.email)
+    if (participantExistis) {
+      throw new InvalidParamError('E-mail já cadastrado')
     }
 
-    async create(participantData : ICreatePaticipantParams): Promise<Participant>{
-        participantData.id = this.idGenerator.createUUID()
-
-        const participantExistis = await this.participantRepository.findByEmail(participantData.email)
-        if(participantExistis){
-            throw new InvalidParamError("E-mail já cadastrado")
-        }  
-        participantData.password = await this.hasher.hash(participantData.password)
-        const participant = await this.participantRepository.create(participantData);
-        return participant;
-
-    }
+    participantData.password = await this.hasher.hash(participantData.password)
+    const participant = await this.participantRepository.create(participantData)
+    return participant
+  }
 }
