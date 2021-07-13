06b48fa7423fea912da414ab00ae898ea69a1628
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-07-13 10:18:55 -0300

    fix: nullable token in users table

--- src/infra/typeorm/migrations/1626180800131-FixNullableDbFields.ts
+++ src/infra/typeorm/migrations/1626180800131-FixNullableDbFields.ts
@@ -4,10 +4,14 @@ export class FixNullableDbFields1626180800131 implements MigrationInterface {
   public async up (queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE public."AuctionItems" ALTER COLUMN "finishedOff" DROP NOT NULL;')
     await queryRunner.query('ALTER TABLE public."AuctionItems" ALTER COLUMN "auctionId" DROP NOT NULL;')
+    await queryRunner.query('ALTER TABLE public."Auctioneer" ALTER COLUMN "token" DROP NOT NULL;')
+    await queryRunner.query('ALTER TABLE public."Participant" ALTER COLUMN "token" DROP NOT NULL;')
   }
 
   public async down (queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE public."AuctionItems" ALTER COLUMN "auctionId" SET NOT NULL;')
     await queryRunner.query('ALTER TABLE public."AuctionItems" ALTER COLUMN "auctionId" SET NOT NULL;')
+    await queryRunner.query('ALTER TABLE public."Auctioneer" ALTER COLUMN "token" SET NOT NULL;')
+    await queryRunner.query('ALTER TABLE public."Participant" ALTER COLUMN "token" SET NOT NULL;')
   }
 }
