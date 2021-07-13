d49982d4a1ecea75cddd6567731240ae2f9beca6
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-07-13 10:25:41 -0300

    fix: typo in table names

--- src/infra/typeorm/migrations/1626180800131-FixNullableDbFields.ts
+++ src/infra/typeorm/migrations/1626180800131-FixNullableDbFields.ts
@@ -4,14 +4,14 @@ export class FixNullableDbFields1626180800131 implements MigrationInterface {
   public async up (queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE public."AuctionItems" ALTER COLUMN "finishedOff" DROP NOT NULL;')
     await queryRunner.query('ALTER TABLE public."AuctionItems" ALTER COLUMN "auctionId" DROP NOT NULL;')
-    await queryRunner.query('ALTER TABLE public."Auctioneer" ALTER COLUMN "token" DROP NOT NULL;')
-    await queryRunner.query('ALTER TABLE public."Participant" ALTER COLUMN "token" DROP NOT NULL;')
+    await queryRunner.query('ALTER TABLE public."Auctioneers" ALTER COLUMN "token" DROP NOT NULL;')
+    await queryRunner.query('ALTER TABLE public."Participants" ALTER COLUMN "token" DROP NOT NULL;')
   }
 
   public async down (queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE public."AuctionItems" ALTER COLUMN "auctionId" SET NOT NULL;')
     await queryRunner.query('ALTER TABLE public."AuctionItems" ALTER COLUMN "auctionId" SET NOT NULL;')
-    await queryRunner.query('ALTER TABLE public."Auctioneer" ALTER COLUMN "token" SET NOT NULL;')
-    await queryRunner.query('ALTER TABLE public."Participant" ALTER COLUMN "token" SET NOT NULL;')
+    await queryRunner.query('ALTER TABLE public."Auctioneers" ALTER COLUMN "token" SET NOT NULL;')
+    await queryRunner.query('ALTER TABLE public."Participants" ALTER COLUMN "token" SET NOT NULL;')
   }
 }
