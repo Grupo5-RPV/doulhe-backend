1555272f222c680923a7ab37d955b8dfc84784f5
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-07-13 16:23:11 -0300

    refact: removing comments

--- src/domain/usecases/auction-item/find-items-by-auction.ts
+++ src/domain/usecases/auction-item/find-items-by-auction.ts
@@ -30,7 +30,6 @@ export default class FindItemsByAuction implements UseCase {
   async execute (id: string): Promise<ItemsByAuctionPresentation> {
     const item = await this.auctionItemRepository.findById(id)
 
-    console.log(item)
     const auction = await this.auctionRepository.findById(item.auctionId)
     const category = await this.categoryRepository.findById(item.categoryId)
     const highestBid = await this.auctionBidRepository.highestBid(item.id)
@@ -44,20 +43,6 @@ export default class FindItemsByAuction implements UseCase {
       highestBid: highestBid ? highestBid.value : item.minimumBid,
       initialBid: item.minimumBid
     } as ItemsByAuctionPresentation
-
-    // for (const item of items) {
-    //   const category = await this.categoryRepository.findById(item.categoryId)
-    //   const highestBid = await this.auctionBidRepository.highestBid(item.id)
-    //   itemsByAuction.push({
-    //     id: item.id,
-    //     name: item.title,
-    //     category: category.name,
-    //     image: item.imagePath,
-    //     finalDate: auction.end,
-    //     highestBid: highestBid.value,
-    //     initialBid: item.minimumBid
-    //   } as ItemsByAuctionPresentation)
-    // }
 
     return itemDto
   }
